424b38e73ecc4597065db2cb9ff892b1ea2bdc98
AM98-20 <alexmrtnz902@gmail.com>
2024-10-15 13:45:22 -0600

    security updates

---
+++ app/main.py
@@ -0,0 +1,43 @@
+from flask import Flask, redirect, render_template
+import firebase_admin
+from firebase_admin import credentials, db
+import os
+
+service_account_info = {
+    "type": "service_account",
+    "project_id": os.environ.get("GOOGLE_CLOUD_PROJECT"),
+    "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
+    "private_key": os.environ.get("PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.environ.get("CLIENT_EMAIL"),
+    "client_id": os.environ.get("CLIENT_ID"),
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-t9z5d%40url-shrinker-client.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+}
+
+cred = credentials.Certificate(service_account_info)
+default_app = firebase_admin.initialize_app(cred_obj,  {
+	'databaseURL': 'https://url-shrinker-client-default-rtdb.firebaseio.com/'
+	})
+
+app = Flask(__name__, static_folder='./build/static', template_folder="./build" )
+
+@app.route("/")
+def hello_world():
+    return redirect("/app")
+
+@app.route("/app")
+def homepage():
+    return render_template('index.html')
+
+@app.route('/<path:generatedKey>', methods=['GET'])
+def fetch_from_firebase(generatedKey):
+    ref = db.reference("/"+ generatedKey)
+    data = ref.get()
+    if not data:
+        return '404 not found'
+    else:
+        longURL = data['longURL']
+        return redirect(longURL)
